71a333e471648f20e3a0a1a25e2acf6b17b27a8e
koreait-shopping <icepri3535@gmail.com>
2022-09-14 15:11:34 +0900

    day 10 JPA Start

--- src/main/java/com/sparata/week02/Week02Application.java
+++ src/main/java/com/sparata/week02/Week02Application.java
@@ -1,7 +1,15 @@
 package com.sparata.week02;
 
+import com.sparata.week02.domain.Course;
+import com.sparata.week02.domain.CourseRepository;
+import com.sparata.week02.domain.Student;
+import com.sparata.week02.domain.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Week02Application {
@@ -9,5 +17,40 @@ public class Week02Application {
     public static void main(String[] args) {
         SpringApplication.run(Week02Application.class, args);
     }
+
+    // Week02Application.java 의 main 함수 아래에 붙여주세요.
+    @Bean
+    public CommandLineRunner demo(CourseRepository repository) {
+        return (args) -> {
+
+            Course course1 = new Course("웹개발의 봄 Spring", "남병관");
+            repository.save(course1);
+
+            List<Course> courseList = repository.findAll();
+            for (int i = 0; i < courseList.size(); i++) {
+                Course c = courseList.get(i);
+                System.out.println(c.getTitle());
+                System.out.println(c.getTutor());
+            }
+
+        };
+    }
+
+//    @Bean
+//    public CommandLineRunner demo(StudentRepository repository) {
+//        return (args) -> {
+//
+//
+//            Student student1 = new Student("주현진", 30);
+//            repository.save(student1);
+//
+//            List<Student> studentList = repository.findAll();
+//            for (int i = 0; i < studentList.size(); i++) {
+//                Student s = studentList.get(i);
+//                System.out.println(s.getName());
+//                System.out.println(s.getAge());
+//            }
+//        };
+//    }
 
 }
---
+++ src/main/java/com/sparata/week02/domain/Student.java
@@ -0,0 +1,32 @@
+package com.sparata.week02.domain;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor // 기본생성자를 대신 생성해준다.
+@Entity // 테이블임을 나타낸다.
+public class Student {
+    @Id // ID 값, Primary Key로 사용하겠다 라는 뜻
+    @GeneratedValue(strategy = GenerationType.AUTO) // 자동 증가 명령
+    private Long id;
+
+    @Column(nullable = false) // 컬럼 값이고 반드시 값이 존재해야 함 을 나타냄
+    private  String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
