e8260e5655ba7c368f9436bf1490da61c343b905
koreait-shopping <icepri3535@gmail.com>
2022-09-15 16:32:07 +0900

    day 11 Lombok, DTO

--- src/main/java/com/sparata/week02/Week02Application.java
+++ src/main/java/com/sparata/week02/Week02Application.java
@@ -1,16 +1,16 @@
 package com.sparata.week02;
 
-import com.sparata.week02.domain.Course;
-import com.sparata.week02.domain.CourseRepository;
-import com.sparata.week02.domain.Student;
-import com.sparata.week02.domain.StudentRepository;
+import com.sparata.week02.domain.*;
+import com.sparata.week02.service.CourseService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.List;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Week02Application {
 
@@ -18,23 +18,32 @@ public class Week02Application {
         SpringApplication.run(Week02Application.class, args);
     }
 
-    // Week02Application.java 의 main 함수 아래에 붙여주세요.
     @Bean
-    public CommandLineRunner demo(CourseRepository repository) {
+    public CommandLineRunner demo(CourseRepository courseRepository, CourseService courseService) {
         return (args) -> {
-
-            Course course1 = new Course("웹개발의 봄 Spring", "남병관");
-            repository.save(course1);
-
-            List<Course> courseList = repository.findAll();
-            for (int i = 0; i < courseList.size(); i++) {
-                Course c = courseList.get(i);
-                System.out.println(c.getTitle());
-                System.out.println(c.getTutor());
+            courseRepository.save(new Course("프론트엔드의 꽃, 리액트", "임민영"));
+            System.out.println("데이터 인쇄");
+            List<Course> courseList = courseRepository.findAll();
+            for (int i=0; i<courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
             }
 
+            CourseRequestDto requestDto = new CourseRequestDto("웹개발의 봄, Spring", "임민영");
+            courseService.update(1L, requestDto);
+            courseList = courseRepository.findAll();
+            for (int i=0; i<courseList.size(); i++) {
+                Course course = courseList.get(i);
+                System.out.println(course.getId());
+                System.out.println(course.getTitle());
+                System.out.println(course.getTutor());
+            }
+            courseRepository.deleteAll();
         };
     }
+
 
 //    @Bean
 //    public CommandLineRunner demo(StudentRepository repository) {
